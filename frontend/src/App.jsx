c63474aa4ceb379c68a444d3fef3cb7e7388ad6b
JoysonStanly <joysonstanley3@gmail.com>
2026-03-28 22:06:34 +0530

    add navbar and hero section UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import Marketplace from './pages/Marketplace';
+import MyListings from './pages/MyListings';
+import ListingDetails from './pages/ListingDetails';
+import ManageListing from './pages/ManageListing';
+import Loading from './pages/Loading';
+import MyOrders from './pages/MyOrders';
+
 
 const App = () => {
+
+  const { pathname } = useLocation();
+
   return (
-    <div>App</div>
+    <div>
+            {!pathname.includes('/admin') && <Navbar />}
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/marketplace' element={<Marketplace />} />
+                <Route path='/my-listings' element={<MyListings />} />
+                <Route path='/listing/:listingId' element={<ListingDetails />} />
+                <Route path='/create-listing' element={<ManageListing />} />
+                <Route path='/edit-listing/:id' element={<ManageListing />} />
+                <Route path='/my-orders' element={<MyOrders />} />
+                <Route path='/loading' element={<Loading />} />
+                
+            </Routes>
+            
+        </div>
   )
 }
 

e22caa628ea069ad9dbc4d63f45dddcc9b320b0c
JoysonStanly <joysonstanley3@gmail.com>
2026-03-29 23:05:29 +0530

    setup clerk for sign in and sign up

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,89 +1,63 @@
 import { assets } from '../assets/assets';
-import {
-  BoxIcon,
-  GripIcon,
-  ListIcon,
-  MenuIcon,
-  MessageCircleMoreIcon,
-  XIcon
-} from 'lucide-react';
+import { UserButton, useUser, useClerk } from '@clerk/clerk-react';
+import { BoxIcon, GripIcon, ListIcon, MenuIcon, MessageCircleMoreIcon, XIcon } from 'lucide-react';
 import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const navigate = useNavigate();
+    const { user } = useUser();
+    const { openSignIn } = useClerk();
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navigate = useNavigate();
 
-  return (
-    <nav className='h-20'>
-      {/* Top Navbar */}
-      <div className='fixed left-0 top-0 right-0 z-50 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white'>
+    return (
+        <nav className='h-20'>
+            <div className='fixed left-0 top-0 right-0 z-100 flex items-center justify-between px-6 md:px-16 lg:px-24 xl:px-32 py-4 border-b border-gray-300 bg-white transition-all'>
+                <img onClick={() => { navigate('/'); scrollTo(0, 0); }} src={assets.logo} alt='logo' className='h-10 cursor-pointer' />
 
-        {/* Logo */}
-        <img
-          onClick={() => {
-            navigate('/');
-            scrollTo(0, 0);
-          }}
-          src={assets.logo}
-          alt='logo'
-          className='h-10 cursor-pointer'
-        />
+                {/* Desktop Menu */}
+                <div className='hidden sm:flex items-center gap-4 md:gap-8 max-md:text-sm text-gray-800'>
+                    <Link onClick={() => scrollTo(0, 0)} to='/'> Home </Link>
+                    <Link onClick={() => scrollTo(0, 0)} to='/marketplace'> Marketplace </Link>
+                    {user ? <Link onClick={() => scrollTo(0, 0)} to='/messages'> Messages </Link> : <Link onClick={openSignIn} to='#'> Messages </Link> }
+                    {user ? <Link onClick={() => scrollTo(0, 0)} to='/my-listings'> My Listings </Link> : <Link onClick={openSignIn} to='#'> My Listings </Link> }
+                </div>
 
-        {/* Desktop Menu */}
-        <div className='hidden sm:flex items-center gap-4 md:gap-8 max-md:text-sm text-gray-800'>
-          <Link to='/' onClick={() => scrollTo(0, 0)}>Home</Link>
-          <Link to='/marketplace' onClick={() => scrollTo(0, 0)}>Marketplace</Link>
-          <Link to='/messages' onClick={() => scrollTo(0, 0)}>Messages</Link>
-          <Link to='/my-listings' onClick={() => scrollTo(0, 0)}>My Listings</Link>
-          <Link to='/my-orders' onClick={() => scrollTo(0, 0)}>My Orders</Link>
-        </div>
+                {!user ? (
+                    <div>
+                        <button onClick={openSignIn} className='max-sm:hidden cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
+                        <MenuIcon className='sm:hidden' onClick={()=>setMenuOpen(true)} />
+                        
+                    </div>
+                ) : (
+                    <UserButton>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label='Marketplace' labelIcon={<GripIcon size={16} />} onClick={() => navigate('/marketplace')} />
+                        </UserButton.MenuItems>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label='Messages' labelIcon={<MessageCircleMoreIcon size={16} />} onClick={() => navigate('/messages')} />
+                        </UserButton.MenuItems>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label='My Listings' labelIcon={<ListIcon size={16} />} onClick={() => navigate('/my-listings')} />
+                        </UserButton.MenuItems>
+                        <UserButton.MenuItems>
+                            <UserButton.Action label='My Orders' labelIcon={<BoxIcon size={16} />} onClick={() => navigate('/my-orders')} />
+                        </UserButton.MenuItems>
+                    </UserButton>
+                )}
+            </div>
 
-        {/* Right Side */}
-        <div>
-          <button
-            onClick={() => alert('Login Clicked')}
-            className='max-sm:hidden cursor-pointer px-6 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full'
-          >
-            Login
-          </button>
-
-          <MenuIcon
-            className='sm:hidden cursor-pointer'
-            onClick={() => setMenuOpen(true)}
-          />
-        </div>
-      </div>
-
-      {/* Mobile Menu */}
-      <div
-        className={`sm:hidden fixed inset-0 ${
-          menuOpen ? 'w-full' : 'w-0'
-        } overflow-hidden bg-white/90 backdrop-blur z-50 text-sm transition-all`}
-      >
-        <div className='flex flex-col items-center justify-center h-full text-xl font-semibold gap-6 p-4'>
-
-          <Link to='/' onClick={() => setMenuOpen(false)}>Home</Link>
-          <Link to='/marketplace' onClick={() => setMenuOpen(false)}>Marketplace</Link>
-          <Link to='/messages' onClick={() => setMenuOpen(false)}>Messages</Link>
-          <Link to='/my-listings' onClick={() => setMenuOpen(false)}>My Listings</Link>
-          <Link to='/my-orders' onClick={() => setMenuOpen(false)}>My Orders</Link>
-
-          <button
-            onClick={() => alert('Login Clicked')}
-            className='px-6 py-2 bg-indigo-500 hover:bg-indigo-600 text-white rounded-full'
-          >
-            Login
-          </button>
-
-          <XIcon
-            onClick={() => setMenuOpen(false)}
-            className='absolute size-8 right-6 top-6 text-gray-500 cursor-pointer'
-          />
-        </div>
-      </div>
-    </nav>
-  );
+            <div className={`sm:hidden fixed inset-0 ${menuOpen ? 'w-full' :'w-0'} overflow-hidden bg-white/70 backdrop-blur shadow-xl rounded-lg z-200 text-sm transition-all`}>
+                <div className='flex flex-col items-center justify-center h-full text-xl font-semibold gap-6 p-4'>
+                    <Link to='/marketplace' onClick={() => setMenuOpen(false)}> Marketplace </Link>
+                    <button onClick={openSignIn}> Messages </button>
+                    <button onClick={openSignIn}> My Listings </button>
+                    <button onClick={openSignIn} className=' cursor-pointer px-8 py-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full'>Login</button>
+                    <XIcon onClick={() => setMenuOpen(false)} className='absolute size-8 right-6 top-6 text-gray-500 hover:text-gray-700 cursor-pointer' />
+                </div>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
